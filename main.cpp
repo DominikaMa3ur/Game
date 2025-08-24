4ab4c46ea53382470cb9975a9d188bcff4b28bfc
Dominika Mazur <92177055+DominikaMa3ur@users.noreply.github.com>
2025-08-24 18:38:59 +0200

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,101 @@
+#include "raylib.h"
+#include <vector>
+
+class GameButton {
+    private:
+        Rectangle rect;
+        Rectangle rect_shadow;
+        const char* text;
+        bool isSelected = false;
+
+        Color color_main = Color{64,64,196,255};
+        Color color_shadow = Color{32,32,128,255};
+        Color color3 = Color{24,24,96,255};
+        Rectangle pressedRect (Rectangle r)
+        {
+            r.x -= 6; r.y += 6;
+            r.width += 12;
+            r.height -= 12;            
+            return r;
+        }
+        void drawButtonText()
+        {
+            DrawText(text, rect.x + rect.width/2 - MeasureText(text, 20)/2, rect.y+rect.height/2+3, 24, color_shadow);            
+            DrawText(text, rect.x + rect.width/2 - MeasureText(text, 20)/2, rect.y+rect.height/2, 24, WHITE);
+        }
+    public:
+        GameButton(int x, int y, int w, int h, const char* GameButtontext = "OK")
+            {rect.x = x; rect.y = y; rect.width = w; rect.height = h; set_rect(rect); text = GameButtontext;}
+        Rectangle get_rect() {return rect;}
+        void center() {rect.x = (GetScreenWidth()-rect.width)/2; set_rect(rect);}
+        void set_rect(Rectangle new_rect) {
+            rect = new_rect; rect_shadow = rect; rect_shadow.y += 4;
+        }
+        bool isInside() {return CheckCollisionPointRec(GetMousePosition(), rect);}
+        bool isPressed() {return (isInside() && IsMouseButtonPressed(MOUSE_BUTTON_LEFT));}
+        void select(bool selected) {isSelected = selected;}
+        void draw() {
+            if ((isInside() && IsMouseButtonDown(MOUSE_BUTTON_LEFT)))
+                {
+                    Rectangle rect4 = pressedRect(rect_shadow);
+                    DrawRectangleRounded(rect4,0.1,4,color3);
+                    Rectangle rect3 = pressedRect(rect);
+                    DrawRectangleRounded(rect3,0.1,4,color_shadow);
+                    drawButtonText();
+                }
+            else if (isSelected)
+                {
+                    //
+                }
+            else if (isInside())
+                {
+                    DrawRectangleRounded(rect_shadow,0.1,4,color_shadow);
+                    Rectangle rect3 = rect;
+                    rect3.y += 2;
+                    DrawRectangleRounded(rect3,0.1,4,color_main);
+                    drawButtonText();
+                }
+            else {
+                    DrawRectangleRounded(rect_shadow,0.1,4,color_shadow);
+                    DrawRectangleRounded(rect,0.1,4,color_main);
+                    drawButtonText();
+                }
+        }
+};
+
+int main ()
+{
+	InitWindow(1280, 800, "3D");
+	
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ -8.0f, 0.0f, 0.0f };
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+    camera.fovy = 45.0f;
+    camera.projection = CAMERA_PERSPECTIVE;
+
+    Vector3 world[10];
+    for (int i = 0; i < 10; i++) {world[i] = {i*4.0f, 0.0f, 0.0f};}
+    std::vector<GameButton> menu = {
+        GameButton(16,16,320,80, "New game"), GameButton(16,128,320,80, "Exit")
+    };
+    menu[0].center();
+    menu[1].center();
+
+	while (!WindowShouldClose())
+	{
+		UpdateCamera(&camera, CAMERA_FIRST_PERSON);
+		BeginDrawing();
+
+		ClearBackground(BLACK);
+        BeginMode3D(camera);
+        for (int i = 0; i < 10; i++) {
+            DrawCube(world[i], 1.0f, 1.0f, 1.0f, BLUE);
+        }
+        EndMode3D();
+        for (int i = 0; i < int(menu.size()); i++) {menu[i].draw();}
+		EndDrawing();
+	}
+	CloseWindow();
+	return 0;
+}
